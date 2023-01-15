86f7d08b9610cc5ab6d83dde4e85ceb99d314638
Upaharika Gawali <upaharikagawali@192.168.1.8>
2023-01-15 15:43:28 +0530

    added class based components

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,21 +1,33 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 import "./style.css";
 
-const AboutUs = () => {
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Construtor : AboutUs");
+  }
+
+  componentDidMount() {
+    console.log("Parent componentDidMount : AboutUs");
+  }
+
+  // Parent componentDidUpdate won't be called as react knows where the update has made i.e. in child component
+  // so only child's componentDidUpdate will be called it is due to virtual DOM
+  componentDidUpdate() {
+    console.log("Parent componentDidUpdate : AboutUsContent");
+  }
+
+  render() {
+    console.log("Parent render : AboutUs");
     return (
       <div className="aboutus-container">
-        <div className="aboutus">
-          <p>
-            <b>Hello Visitor! 👋</b>
-          </p>
-          <p>We're 13 Web Pirates from Namaste React Bootcamp.</p>
-          <p>
-            We're here to make us a better react developer by learning from the
-            best trainer <br/><b>Akshay Saini.</b> 🥷
-          </p>
-          <p>Happy Coding and obviously with a Cup of Coffee. ☕</p>
-        </div>
+        <p>
+          <b>Hello Visitor! 👋</b>
+        </p>
+        <Outlet />
       </div>
     );
+  }
 }
-
 export default AboutUs;
